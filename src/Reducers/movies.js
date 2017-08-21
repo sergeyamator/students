737f60321159a348b2179c602a53b47f58844c15
sergey <sramator@gmail.com>
2017-08-21 09:13:43 +0300

    Init

---
+++ src/Reducers/movies.js
@@ -0,0 +1,1 @@
+const initialState = {isFetching: false, items: []};
---
+++ src/Reducers/movies.test.js
@@ -0,0 +1,9 @@
+import moviesReducer from './movies';
+
+describe('Movies reducer', () => {
+  describe('should set initial settings', () => {
+
+  });
+});
+
+const initialState = {isFetching: false, items: []};
