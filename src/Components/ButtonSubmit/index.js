6403580819e64aaec497576d750b4e0428473201
Vika Kovalenko <kvvendetta@gmail.com>
2017-10-10 19:37:35 +0300

    Changed form.

--- src/Components/ButtonSubmit/index.js
+++ src/Components/ButtonSubmit/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../../styles.scss';
+import './style.scss';
 
 const ButtonSubmit = props => (
   <button
--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -2,27 +2,38 @@ import React from 'react';
 import ButtonClear from '../ButtonClear/index';
 import ButtonSubmit from '../ButtonSubmit/index';
 import Checkbox from '../Checkbox/index';
-import '../../styles.scss';
+import './style.scss';
 
 const Form = () => (
   <form
     method="post"
     action="#"
+    className="log-in"
   >
-    <input
-      type="email"
-      className="email"
-      name="email"
-      placeholder="Введите свой email"
-    />
-    <input
-      type="password"
-      name="password"
-      className="password"
-    />
+    <h1 className="description">Войти в личный кабинет</h1>
+    <div className="log-in__field">
+      <input
+        type="email"
+        className="log-in-input email"
+        name="email"
+        placeholder="Введите свой email"
+        required
+      />
+    </div>
+    <div className="log-in__field">
+      <input
+        type="password"
+        name="password"
+        className="log-in-input password"
+        required
+        placeholder="Введите свой пароль"
+      />
+    </div>
+    <div className="log-in__button">
+      <ButtonSubmit>Войти</ButtonSubmit>
+      <ButtonClear>Очистить</ButtonClear>
+    </div>
     <Checkbox label="Я новый пользователь" />
-    <ButtonSubmit>Отправить</ButtonSubmit>
-    <ButtonClear>Очистить форму</ButtonClear>
   </form>
 );
 
