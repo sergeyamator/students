f4f46309e29c5f655d69d9c5540ca329e5109ca5
Vika Kovalenko <kvvendetta@gmail.com>
2017-10-10 13:56:29 +0300

    Replaced the value of the button  with a props. Add form.

--- src/Components/ButtonClear/index.js
+++ src/Components/ButtonClear/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../../styles.scss';
 
-const ButtonClear = () => (
+const ButtonClear = props => (
   <button
     className="button-clear"
     type="reset"
-  >Очистить
+  >
+    {props.children}
   </button>
 );
+
+ButtonClear.propTypes = {
+  children: PropTypes.string.isRequired,
+};
 
 export default ButtonClear;
