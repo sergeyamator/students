28ac40c0d2b5eba50f2e26b3123b3f92a302df21
Vika Kovalenko <kvvendetta@gmail.com>
2017-10-10 12:32:56 +0300

    Add new component for Button.

---
+++ src/Components/ButtonClear/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles.scss';
+
+const ButtonClear = () => (
+  <button
+    className="button-clear"
+    type="reset"
+  >Очистить
+  </button>
+);
+
+export default ButtonClear;
---
+++ src/Components/ButtonSubmit/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles.scss';
+
+const ButtonSubmit = () => (
+  <button
+    className="button-submit"
+    type="submit"
+  >Отправить
+  </button>
+);
+
+export default ButtonSubmit;
