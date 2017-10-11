605674326c74c44ab7472ca4ad302e377b274fd7
Serhii Rubets <serhii_rubets@epam.com>
2017-10-11 17:13:05 +0300

    Json parser was added

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,18 @@ const bodyParser = require('body-parser');
 // создаем объект приложения
 const app = express();
 
-// создаем парсер для данных application/x-www-form-urlencoded
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+// создаем парсер для данных в формате json
+const jsonParser = bodyParser.json();
+
 app.use(express.static(`${__dirname}/public`));
 
-app.post('/register', urlencodedParser, (request, response) => {
+app.post('/register', jsonParser, (request, response) => {
   if (!request.body) {
-    return response.sendStatus(400)
+    return response.sendStatus(400);
   }
 
   console.log(request.body);
-  return response.send(`${request.body.userName} - ${request.body.userAge}`);
+  return response.json(`${request.body.userName} - ${request.body.userAge}`);
 });
 // определяем обработчик для маршрута '/'
 app.get('/', (request, response) => {
