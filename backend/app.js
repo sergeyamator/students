51a5c44be18952eae23732fdc7bb9ad27ad4f36e
sergey <sramator@gmail.com>
2018-11-27 17:24:32 +0200

    Remove passport

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const registerController = require('./controllers/mentor/register');
-
+const expressjwt = require('express-jwt');
+require('dotenv').config();
+// const registerController = require('./controllers/mentor/register');
 const loginController = require('./controllers/mentor/login');
 const studentsController = require('./controllers/students');
 
-require('dotenv').config();
+
+const jwtCheck = expressjwt({
+  secret: process.env.SECRET,
+});
+
+
 require('./config/db/mongoose');
 
 const app = express();
@@ -17,21 +19,8 @@ const app = express();
 app.use(express.static('../public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cookieParser(process.env.SECRET_COOKIE));
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  secret: process.env.SECRET,
-  store: new MongoStore({ url: process.env.DATABASE }),
-}));
 
-require('./config/passport');
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/register', registerController);
 app.use('/login', loginController);
-app.use('/students', studentsController);
-app.use('/', (req, res) => res.send('MAIN PAGE'));
-app.listen(process.env.PORT);
+app.use('/students', jwtCheck, studentsController);
+
+app.listen(process.env.PORT || 3000);
--- backend/controllers/mentor/login.js
+++ backend/controllers/mentor/login.js
@@ -1,21 +1,41 @@
 const express = require('express');
-const passport = require('passport');
+const jwt = require('jsonwebtoken');
 require('../../models/mentor');
 
+
 const router = express.Router();
+const userData = {
+  name: 'admin',
+  password: 'admin',
+  id: 123424,
+};
 
 
 router.get('/', (req, res) => {
-  if (req.isAu)
   res.send('GET ON LOGIN PAGE');
 });
 
-router.post('/', (req, res, done) => {
-  passport.authenticate('local', (_err, user) => {
-    req.login(user, (err) => {
-      res.send('Вы удачно прошли аутентификацию!');
-    });
-  })(req, res, done);
+router.post('/', (req, res) => {
+  const { name, password } = req.body;
+  if (!name || !password) {
+    res.status(400);
+    res.send('You did not provide username or password');
+    return;
+  }
+
+  if (name !== userData.name && password !== userData.password) {
+    res.status(401);
+    res.send('Name or password are wrong');
+    return;
+  }
+
+  const token = jwt.sign({
+    sub: userData.id,
+    name: userData.name,
+  }, process.env.SECRET);
+
+  res.status(200);
+  res.json(token);
 });
 
 
