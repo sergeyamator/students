b00c995cd08150cd0f9d323f5ea89c42d0bc1cdf
Vikulchik <vikulchik>
2018-12-04 20:36:23 +0200

    Change webpack config

--- backend/api/mentor/index.js
+++ backend/api/mentor/index.js
@@ -7,74 +7,71 @@ const Mentor = require('../../models/mentor');
 const { JWT_SECRET } = require('../../config');
 
 const multerConfig = {
-  storage: multer.diskStorage({
-    destination(req, res, next) {
-      next(null, path.join(__dirname, '../../public/img/avatar'));
-    },
-    filename(req, file, next) {
-      const splittedName = file.originalname.split('.');
-      const fileName = `${req.body.email}-avatar.${splittedName[splittedName.length - 1]}`;
-      const directory = path.join(__dirname, '../../public/img/avatar');
-      const files = fs.readdirSync(directory);
+    storage: multer.diskStorage({
+        destination(req, res, next) {
+            next(null, path.join(__dirname, '../../public/img/avatar'));
+        },
+        filename(req, file, next) {
+            const splittedName = file.originalname.split('.');
+            const fileName = `${req.body.email}-avatar.${splittedName[splittedName.length - 1]}`;
+            const directory = path.join(__dirname, '../../public/img/avatar');
+            const files = fs.readdirSync(directory);
 
-      files.forEach((currentFile) => {
-        if (currentFile.startsWith(req.body.email)) {
-          fs.unlinkSync(path.join(directory, currentFile));
-          next(null, fileName);
-        } else {
-          next(null, fileName);
-        }
-      });
-    },
-    fileFilter(req, file, next) {
-      const image = file.mimetype.startsWith('image/');
+            files.forEach((currentFile) => {
+                if (currentFile.startsWith(req.body.email)) {
+                    fs.unlinkSync(path.join(directory, currentFile));
+                    next(null, fileName);
+                } else {
+                    next(null, fileName);
+                }
+            });
+        },
+        fileFilter(req, file, next) {
+            const image = file.mimetype.startsWith('image/');
 
-      if (image) {
-        return next(null, true);
-      }
+            if (image) {
+                return next(null, true);
+            }
 
-      return next({ message: 'File type not supported' }, false);
-    },
-  }),
+            return next({ message: 'File type not supported' }, false);
+        },
+    }),
 };
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const authorizationHeader = req.get('Authorization');
+    const authorizationHeader = req.get('Authorization');
 
-  if (authorizationHeader) {
-    const token = authorizationHeader.split(' ')[1];
+    if (authorizationHeader) {
+        const token = authorizationHeader.split(' ')[1];
 
-    jwt.verify(token, JWT_SECRET, async (err, decoded) => {
-      try {
-        const mentor = await Mentor.findById(decoded.sub);
+        jwt.verify(token, JWT_SECRET, async(err, decoded) => {
+            try {
+                const mentor = await Mentor.findById(decoded.sub);
 
-        res.json({
-          id: mentor._id,
-          email: mentor.email,
-          username: mentor.username,
-          createdAt: mentor.created_at,
-          avatar: mentor.avatar,
-          technologies: mentor.technologies,
+                res.json({
+                    id: mentor._id,
+                    email: mentor.email,
+                    username: mentor.username,
+                    createdAt: mentor.created_at,
+                    avatar: mentor.avatar,
+                    technologies: mentor.technologies,
+                });
+            } catch (e) {
+                res.status(500);
+                res.json(e);
+            }
         });
-      } catch (e) {
-        res.status(500);
-        res.json(e);
-      }
-    });
-  }
+    }
 });
 
-router.post('/', multer(multerConfig).single('avatar'), async (req, res) => {
-  const avatar = req.file && req.file.filename || '';
-  const mentor = await Mentor.findOneAndUpdate(
-    { email: req.body.email },
-    { $set: { ...req.body, avatar } },
-    { new: true },
-  );
+router.post('/', multer(multerConfig).single('avatar'), async(req, res) => {
+    console.log(req.file)
+    const avatar = req.file && req.file.filename || '';
+    const mentor = await Mentor.findOneAndUpdate({ email: req.body.email }, { $set: {...req.body, avatar } }, { new: true }, );
 
-  res.json(mentor);
+    res.json(mentor);
 });
 
 module.exports = router;
--- webpack.config.js
+++ webpack.config.js
@@ -10,56 +10,56 @@ const PORT = process.env.PORT || '8888';
 
 
 module.exports = {
-  entry: [
-    './frontend/index.js', // your app's entry point
-  ],
-  devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
-  output: {
-    publicPath: '/',
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    loaders,
-  },
-  devServer: {
-    contentBase: './public',
-    // do not print bundle build stats
-    noInfo: true,
-    // enable HMR
-    hot: true,
-    // embed the webpack-dev-server runtime into the bundle
-    inline: true,
-    // serve index.html in place of 404 responses to allow HTML5 history
-    historyApiFallback: true,
-    port: PORT,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
-      },
-      '/img': {
-        target: 'http://localhost:3000/',
-      },
+    entry: [
+        './frontend/index.js', // your app's entry point
+    ],
+    devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
+    output: {
+        publicPath: '/',
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
     },
-    host: HOST,
-  },
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin({
-      filename: 'style.css',
-      allChunks: true,
-    }),
-    new DashboardPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/template.html',
-      files: {
-        css: ['style.css'],
-        js: ['bundle.js'],
-      },
-    }),
-  ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        loaders,
+    },
+    devServer: {
+        contentBase: './public',
+        // do not print bundle build stats
+        noInfo: true,
+        // enable HMR
+        hot: true,
+        // embed the webpack-dev-server runtime into the bundle
+        inline: true,
+        // serve index.html in place of 404 responses to allow HTML5 history
+        historyApiFallback: true,
+        port: PORT,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000/',
+            },
+            '/img': {
+                target: 'http://localhost:3000/',
+            },
+        },
+        host: HOST,
+    },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin({
+            filename: 'style.css',
+            allChunks: true,
+        }),
+        new DashboardPlugin(),
+        new HtmlWebpackPlugin({
+            template: './frontend/template.html',
+            files: {
+                css: ['style.css'],
+                js: ['bundle.js'],
+            },
+        }),
+    ],
 };
